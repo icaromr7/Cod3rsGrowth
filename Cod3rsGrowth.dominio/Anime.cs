f3069e3de969cf887144784e13e9360aff1a4209
unknown <Icaro Eduardo>
2024-05-13 13:59:26 -0300

    Subindo para continuar em casa

---
+++ Cod3rsGrowth.dominio/Anime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.dominio
+{
+    internal class Anime
+    {
+        private long id;
+        private string nome;
+        private List<Genero> generos;
+        private DateTime data_lancamento;
+        private enum status { };
+
+        public Anime(string nome, List<Genero> generos, DateTime data_lancamento)
+        {
+            this.Nome = nome;
+            this.Generos = generos;
+            this.data_lancamento = data_lancamento;
+        }
+
+        public DateTime Data_lancamento { get => data_lancamento; set => data_lancamento = value; }
+        internal List<Genero> Generos { get => generos; set => generos = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public long Id { get => id; set => id = value; }
+    }
+}
---
+++ Cod3rsGrowth.dominio/Genero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.dominio
+{
+    internal class Genero
+    {
+        private long id;
+        private string nome;
+
+        public Genero(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public long getId() { return id; }
+        public string getNome() {  return nome; }
+
+        public void setNome(string nome) {  this.nome = nome; }
+
+        public void setId(long id) { this.id = id;}
+        
+        public string toString() { return nome; }
+
+    }
+}
