5ece64f7f7ad7caf6963abfeca45a652dfb4a3d3
unknown <Icaro Eduardo>
2024-05-14 10:27:20 -0300

    Correções em Anime realizadas e adicionado descrições em Statusde exibição

--- Cod3rsGrowth.dominio/Anime.cs
+++ Cod3rsGrowth.dominio/Anime.cs
@@ -12,17 +12,17 @@ namespace Cod3rsGrowth.dominio
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sinopse { get; set; }
-        public List<int> GenerosId { get; set; } = new List<int>();
+        public List<int> GenerosIds { get; set; } = new List<int>();
         public DateTime DataLancamento { get; set; }
         public decimal Nota { get; set; }
         public StatusDeExibicao StatusDeExibicao { get; set; }
 
-        public Anime(int id, string nome, string sinopse, List<int> generosId, DateTime dataLancamento, decimal nota, StatusDeExibicao statusDeExibicao)
+        public Anime(int id, string nome, string sinopse, List<int> generosIds, DateTime dataLancamento, decimal nota, StatusDeExibicao statusDeExibicao)
         {
             this.Id = id;
             this.Nome = nome;
             this.Sinopse = sinopse;
-            this.GenerosId = generosId;
+            this.GenerosIds = generosIds;
             this.DataLancamento = dataLancamento;
             this.Nota = Nota;
             this.StatusDeExibicao = statusDeExibicao;
