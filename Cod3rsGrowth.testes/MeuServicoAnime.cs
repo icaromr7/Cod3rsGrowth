039f4aea51bac61b98d2357b1f189d4eef584103
unknown <Icaro Eduardo>
2024-05-14 13:01:53 -0300

    Criado as classes para os testes com injeção

---
+++ Cod3rsGrowth.testes/MeuServicoAnime.cs
@@ -0,0 +1,51 @@
+﻿using Cod3rsGrowth.dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.testes
+{
+    public class MeuServicoAnime : AnimeServico
+    {
+
+        public MeuServicoAnime()
+        {
+        }
+
+        public string alterarAnime(Anime anime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Anime buscarAnime(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Anime buscartodosAnime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string deletarAnime(Anime anime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string deletarAnimePorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Anime GetAnime()
+        {
+            throw new NotImplementedException();
+        }
+        public string inserirAnime(Anime anime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
