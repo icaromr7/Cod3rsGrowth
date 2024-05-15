8eb364e775cdbd2ccc2c542c4a4e97cb4282017d
unknown <Icaro Eduardo>
2024-05-15 14:00:27 -0300

    Iniciando a criação das classes

---
+++ Cod3rsGrowth.testes/TabelaDeAnimes.cs
@@ -0,0 +1,19 @@
+﻿using Cod3rsGrowth.dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.testes
+{
+    public sealed class TabelaDeAnimes
+    {
+        private static readonly TabelaDeAnimes instance = new TabelaDeAnimes();
+
+        public List<Anime> Animes { get { new Anime(1, "Anime1", "Sinopse1", List<int>generosIds, )} }
+
+        private TabelaDeAnimes() { }
+        public static TabelaDeAnimes Instance {  get { return instance; } }
+    }
+}
---
+++ Cod3rsGrowth.testes/TabelaDeGeneros.cs
@@ -0,0 +1,25 @@
+﻿using Cod3rsGrowth.dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.testes
+{
+    public sealed class TabelaDeGeneros
+    {
+        private static readonly TabelaDeGeneros instance = new TabelaDeGeneros();
+        public List<Genero> Generos
+        {
+            get { Generos.Add(new Genero(1, "Acao"));
+                  Generos.Add(new Genero(2, "Aventura"));
+                  Generos.Add(new Genero(3, "Drama"));
+                  Generos.Add(new Genero(4, "Comedia"));
+                return Generos; }
+            }
+        private TabelaDeGeneros() { }
+        public static TabelaDeGeneros Instance { 
+            get { return instance; } }
+    }
+}
