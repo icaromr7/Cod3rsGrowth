8ef61d8f95d92877a17dc000876537639783fbf1
Icaro <icaromr23@hotmail.com>
2024-05-15 16:12:49 -0300

    Criado parte do Singleton no TabelaDeGeneros

--- Cod3rsGrowth.testes/TabelaDeAnimes.cs
+++ Cod3rsGrowth.testes/TabelaDeAnimes.cs
@@ -11,7 +11,7 @@ namespace Cod3rsGrowth.testes
     {
         private static readonly TabelaDeAnimes instance = new TabelaDeAnimes();
 
-        public List<Anime> Animes { get { new Anime(1, "Anime1", "Sinopse1", List<int>generosIds, )} }
+        private static List<Anime> Animes { get { new Anime(1, "Anime1", "Sinopse1", new List<int>() { 1,2 }, new DateTime(2024, 5, 15), 7.8m, Anime.Status.EmExibicao); return Animes; } }
 
         private TabelaDeAnimes() { }
         public static TabelaDeAnimes Instance {  get { return instance; } }
--- Cod3rsGrowth.testes/TabelaDeGeneros.cs
+++ Cod3rsGrowth.testes/TabelaDeGeneros.cs
@@ -9,17 +9,28 @@ namespace Cod3rsGrowth.testes
 {
     public sealed class TabelaDeGeneros
     {
-        private static readonly TabelaDeGeneros instance = new TabelaDeGeneros();
-        public List<Genero> Generos
-        {
-            get { Generos.Add(new Genero(1, "Acao"));
-                  Generos.Add(new Genero(2, "Aventura"));
-                  Generos.Add(new Genero(3, "Drama"));
-                  Generos.Add(new Genero(4, "Comedia"));
-                return Generos; }
-            }
+        private static TabelaDeGeneros instance = null;
+        private static readonly object locker = new object();
+        private static List<Genero> _generos = new List<Genero>();
+
         private TabelaDeGeneros() { }
         public static TabelaDeGeneros Instance { 
-            get { return instance; } }
+            get {
+                if(instance == null)
+                {
+                    instance = new TabelaDeGeneros();
+                }
+                return instance; } 
+        }
+        public static List<Genero> ObterGeneros()
+        {
+            lock (locker) {
+                _generos.Add(new Genero(1, "Acao"));
+                _generos.Add(new Genero(2, "Aventura"));
+                _generos.Add(new Genero(3, "Drama"));
+                _generos.Add(new Genero(4, "Comedia"));
+                return _generos;
+            }
+        }
     }
 }
