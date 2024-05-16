6b54b4c6e33095d5f28de9983ff6351cc72c16f0
unknown <Icaro Eduardo>
2024-05-16 13:50:36 -0300

    Alterado o nome das variaveis em cada teste

--- Cod3rsGrowth.testes/ObterDadosDaTabelaTeste.cs
+++ Cod3rsGrowth.testes/ObterDadosDaTabelaTeste.cs
@@ -7,32 +7,32 @@ namespace Cod3rsGrowth.testes
         [Fact]
         public void ObterDadosDaTabelaGenero_1_acao()
         {
-            List<Genero> tabelaDeGeneros = new List<Genero>();
-            tabelaDeGeneros.Add(new Genero(1, "Acao"));
+            List<Genero> tabelaDeGenerosEsperada = new List<Genero>();
+            tabelaDeGenerosEsperada.Add(new Genero(1, "Acao"));
 
             //arrange
             GeneroRepositorioMock generoRepositorioMock = new GeneroRepositorioMock();
 
             //act
-            List<Genero> tabela_de_generos = generoRepositorioMock.ObterTodos();
+            List<Genero> tabelaDeGenerosAtual = generoRepositorioMock.ObterTodos();
 
             //assert
-            ComparacaoGenero.Equals(tabela_de_generos, tabelaDeGeneros);
+            ComparacaoGenero.Equals(tabelaDeGenerosEsperada,tabelaDeGenerosAtual);
         }
         [Fact]
         public void ObterDadosDaTabelaAnime()
         {
-            List<Anime> tabelaDeAnimes = new List<Anime>();
-            tabelaDeAnimes.Add(new Anime(1, "Anime1", "Sinopse1", new List<int>() { 1, 2 }, new DateTime(2024, 5, 15), 7.8m, Anime.Status.EmExibicao));
+            List<Anime> tabelaDeAnimesEsperada = new List<Anime>();
+            tabelaDeAnimesEsperada.Add(new Anime(1, "Anime1", "Sinopse1", new List<int>() { 1, 2 }, new DateTime(2024, 5, 15), 7.8m, Anime.Status.EmExibicao));
 
             // arrange
             AnimeRepositorioMock animeRepositorioMock = new AnimeRepositorioMock();
 
             //act
-            List<Anime> tabela_de_animes = animeRepositorioMock.ObterTodos();
+            List<Anime> tabelaDeAnimesAtual = animeRepositorioMock.ObterTodos();
 
             //assert
-            ComparacaoAnime.Equals(tabelaDeAnimes,tabela_de_animes);
+            ComparacaoAnime.Equals(tabelaDeAnimesEsperada, tabelaDeAnimesAtual);
             
         }
     }  
