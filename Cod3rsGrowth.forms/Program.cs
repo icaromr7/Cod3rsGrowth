7deb8c6a34881d3671e4eb2179136c541513cab7
Icaro <icaromr23@hotmail.com>
2024-06-04 09:07:01 -0300

    Criada a conexão com o banco de dados

--- Cod3rsGrowth.forms/Program.cs
+++ Cod3rsGrowth.forms/Program.cs
@@ -1,3 +1,5 @@
+using System.Configuration;
+
 namespace Cod3rsGrowth.forms
 {
     internal static class Program
@@ -8,8 +10,8 @@ namespace Cod3rsGrowth.forms
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            var appSettings = ConfigurationManager.AppSettings;
+            string result = appSettings["ConnectionString"];
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
