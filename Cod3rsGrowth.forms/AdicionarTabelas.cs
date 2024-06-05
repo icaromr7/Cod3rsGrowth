256d6c4692ce077dba847405f3d333cf5ff3681d
Icaro <icaromr23@hotmail.com>
2024-06-05 09:03:05 -0300

    Metodos para criação da tabela e tabelas criadas no BD

---
+++ Cod3rsGrowth.forms/AdicionarTabelas.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+using System.Windows.Forms;
+using System;
+
+namespace Cod3rsGrowth.forms
+{
+    [Migration(20240605085700)]
+    public class AdicionarTabelas : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Anime");
+            Delete.Table("Genero");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Anime")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Nome").AsString()
+                .WithColumn("Sinopse").AsString()
+                .WithColumn("DataLancamento").AsDateTime()
+                .WithColumn("GenerosIds").AsInt32().NotNullable()
+                .WithColumn("Nota").AsDecimal()
+                .WithColumn("StatusDeExibicao").AsString();
+            Create.Table("Genero")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Nome").AsString();
+            Create.ForeignKey("fk_Anime_GeneroIds_GeneroId")
+            .FromTable("Anime").ForeignColumn("GenerosIds")
+            .ToTable("Genero").PrimaryColumn("Id");
+        }
+    }
+}
--- Cod3rsGrowth.forms/Program.cs
+++ Cod3rsGrowth.forms/Program.cs
@@ -1,17 +1,39 @@
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
 using System.Configuration;
 
 namespace Cod3rsGrowth.forms
 {
-    internal static class Program
+    public static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main(string[] args)
         {
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            using (var serviceProvider = CreateServices())
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new Form1());
+        }
+        private static ServiceProvider CreateServices()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            string result = appSettings["ConnectionString"];
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString(result)
+                    .ScanIn(typeof(AdicionarTabelas).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
         }
     }
 }
