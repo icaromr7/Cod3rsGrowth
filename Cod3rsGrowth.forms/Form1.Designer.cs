8ae6f364fc9dda0e42f1d30ece0bb87a9a0224f8
Icaro <icaromr23@hotmail.com>
2024-06-05 11:58:48 -0300

    Injeção de dependencia

--- Cod3rsGrowth.forms/Form1.Designer.cs
+++ Cod3rsGrowth.forms/Form1.Designer.cs
@@ -1,16 +1,12 @@
-﻿namespace Cod3rsGrowth.forms
-{
-    partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+﻿using Cod3rsGrowth.dominio;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+namespace Cod3rsGrowth.forms
+{
+    public partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,19 +17,20 @@
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+        { 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label;
     }
 }
--- Cod3rsGrowth.forms/Form1.cs
+++ Cod3rsGrowth.forms/Form1.cs
@@ -1,10 +1,12 @@
+using Cod3rsGrowth.dominio;
+
 namespace Cod3rsGrowth.forms
 {
     public partial class Form1 : Form
     {
         public Form1()
-        {
-            InitializeComponent();
+        {           
+            InitializeComponent();  
         }
     }
 }
